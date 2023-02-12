3572e7fe9498ab23e43e462efa9a994f7f97dd68
Dion Almaer <dion@almaer.com>
2023-02-11 20:04:54 -0700

    Moar tests
    Embeddings, completions, ...

--- tests/index.js
+++ tests/index.js
@@ -1,56 +1,73 @@
-import test from 'ava';
+import test from "ava";
 import { Polymath } from "../main.js";
 
-test('Polymath requires an OpenAI API Key', t => {
-	try {
-		let p = new Polymath({
-			apiKey: "sk-fake-api-key",
-			libraryFiles: ['./libraries/knowledge-string.json']
-		});
+test("Polymath requires an OpenAI API Key", (t) => {
+  try {
+    let p = new Polymath({
+      apiKey: "sk-fake-api-key",
+      libraryFiles: ["./libraries/knowledge-string.json"],
+    });
 
-		t.pass();
-	} catch (e) {
-		t.fail();
-	}
+    t.pass();
+  } catch (e) {
+    t.fail();
+  }
 });
 
-test('Polymath errors without an OpenAI API Key', t => {
-	try {
-		new Polymath();
-		t.fail();
-	} catch (e) {
-		t.pass();
-	}
+test("Polymath errors without an OpenAI API Key", (t) => {
+  try {
+    new Polymath();
+    t.fail();
+  } catch (e) {
+    t.pass();
+  }
 });
 
-test('Polymath gets results', async t => {
-	try {
-		let p = new Polymath({
-			apiKey: process.env.OPENAI_API_KEY,
-			libraryFiles: ['./libraries/knowledge-string.json']
-		});
+test("Polymath gets results", async (t) => {
+  try {
+    let p = new Polymath({
+      apiKey: process.env.OPENAI_API_KEY,
+      libraryFiles: ["./libraries/knowledge-string.json"],
+    });
 
-		let r = await p.results("How long is a piece of string?");
+    let r = await p.results("How long is a piece of string?");
 
-		if (r.context()) {
-			t.pass();
-		}
-	} catch (e) {
-		t.fail();
-	}
+    if (r.context()) {
+      t.pass();
+    }
+  } catch (e) {
+    t.fail();
+  }
 });
-//
-// console.log("OpenAI API Key:", p.apiKey);
 
-// -- Ask our Polymath to do his job and return his bits and pieces
-// let r = await p.results("How long is a piece of string?");
-// console.log("Context: ", r.context());
-// console.log("Results: ", r);
+test("Polymath gets completions", async (t) => {
+  try {
+    let p = new Polymath({
+      apiKey: process.env.OPENAI_API_KEY,
+      libraryFiles: ["./libraries/knowledge-string.json"],
+    });
 
-// -- Ask our Polymath to do us a favor and get hits bits and pieces and use them to query OpenAI
-// let cr = await p.completion("How long is a piece of string?");
-// console.log("Results: ", cr);
+    let r = await p.completion("How long is a piece of string?");
 
-// -- Oh, you can also just get an embedding from a string
-// let embedding = await p.generateEmbedding("DION");
-// console.log("Embedding: ", embedding);
+    if (r.completion) {
+      t.pass();
+    }
+  } catch (e) {
+    t.fail();
+  }
+});
+
+test("Polymath can get embeddings", async (t) => {
+  let p = new Polymath({
+    apiKey: process.env.OPENAI_API_KEY,
+    libraryFiles: ["./libraries/knowledge-string.json"],
+  });
+
+  let embedding = await p.generateEmbedding("ePiano");
+
+  if (embedding.length == 1536) {
+	t.pass();
+  } else {
+	t.fail();
+  }
+});
