e8fe57da604a2973fe91586532d8dbaa7f39dd60
Dion Almaer <dion@almaer.com>
2023-02-24 19:20:59 -0700

    Ok, some types
    I was being lazy and waiting for the config store... but may as well get the type in there.

--- app/utils/polymath.config.js
+++
@@ -1,5 +0,0 @@
-import polymathHostConfig from "~/host.SECRET.json";
-
-export { polymathHostConfig };
-
-// TODO: don't just return some JSON, but have a wrapper using the JSON store that returns DEFAULTS etc
---
+++ app/utils/polymath.config.ts
@@ -0,0 +1,44 @@
+// TODO: don't just return some JSON, but have a wrapper using the JSON store that returns DEFAULTS etc
+
+import polymathHostConfigJson from "~/host.SECRET.json";
+
+const polymathHostConfig: PolymathHostConfig = polymathHostConfigJson as PolymathHostConfig;
+
+interface PolymathHostConfig {
+    default_private_access_tag: string;
+    default_api_key: string;
+    endpoint: string;
+    client_options: {
+        pinecone: {
+            apiKey: string;
+            baseUrl: string;
+            namespace: string;
+            topK: number;
+        };
+        omit: string;
+        debug: boolean;
+    };
+    completions_options: {
+        model: string;
+        prompt_template: string;
+        max_tokens: number;
+        temperature: number;
+        top_p: number;
+        n: number;
+        stream: boolean;
+        logprobs: null;
+        debug: boolean;
+    };
+    info: {
+        headername: string;
+        placeholder: string;
+        fun_queries: string[];
+        source_prefixes: {
+            [key: string]: string;
+        };
+    };
+    
+}
+
+export { polymathHostConfig };
+export type { PolymathHostConfig };
