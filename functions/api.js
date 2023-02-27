b03754028a9dbedbad24d4674bf88cf43cb82d43
Dimitri Glazkov <dimitri@glazkov.com>
2023-02-26 17:37:09 -0800

    Smallest possible working thing.

---
+++ functions/api.js
@@ -0,0 +1,25 @@
+import functions from 'firebase-functions';
+import { defineSecret } from 'firebase-functions/params';
+import express from 'express';
+import cors from 'cors';
+
+import { Polymath } from '@polymath-ai/client';
+
+const app = express();
+
+const openai_api_key = defineSecret('OPENAI_API_KEY');
+
+app.use(cors({ origin: true }));
+
+app.get('/api', async (req, res) => {
+  const polymath = new Polymath({
+    apiKey: openai_api_key.value(),
+    servers: [ 'https://polymath.glazkov.com/ '],
+  });
+  const results = await polymath.completion('What is a customer-vendor loop?')
+  res.json(results);
+});
+
+const api = functions.runWith({ openai_api_key }).https.onRequest(app);
+
+export { api }
