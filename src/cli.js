a0cf84490453810d11ecdac47a1f2559697a7246
Dimitri Glazkov <dimitri@glazkov.com>
2023-02-28 21:53:27 -0800

    Get out of global scope.

--- src/cli.js
+++ src/cli.js
@@ -16,137 +16,151 @@ import { Command, Option } from "commander";
 //
 // CONFIGURE OPTIONS
 //
-const program = new Command();
 
-// Get the description and version from our own package.json
-const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+class CLI {
+  program
 
-program
-  .name("polymath")
-  .description(packageJson.description)
-  .version(packageJson.version);
-
-program.option("-d, --debug", "output extra debugging");
-program.option("-c, --config <path>", "config file");
-program.addOption(
-  new Option("--openai-api-key <key>", "OpenAI API key").env("OPENAI_API_KEY")
-);
-program.option(
-  "-s, --server <endpoint>",
-  "Polymath server endpoint",
-  collect,
-  []
-);
-program.option(
-  "-l, --libraries <libOrDirectory>",
-  "Library files or directory"
-);
-
-program.option("-p, --pinecone", "Use pinecone");
-program.addOption(
-  new Option("--pinecone-api-key <key>", "pinecone api key").env(
-    "PINECONE_API_KEY"
-  )
-);
-program.addOption(
-  new Option("--pinecone-base-url <url>", "pinecone base url").env(
-    "PINECONE_BASE_URL"
-  )
-);
-program.addOption(
-  new Option("--pinecone-namespace <namespace>", "pinecone namespace").env(
-    "PINECONE_NAMESPACE"
-  )
-);
-
-//
-// SUB COMMANDS
-//
-
-program
-  .command("ask")
-  .description("Ask a question to a Polymath")
-  .argument("[question]", "The question to ask")
-  .action(askOrComplete);
-
-program
-  .command("complete")
-  .description("Ask a Polymath for a completion")
-  .argument("[question]", "The question to ask")
-  .alias("completion")
-  .action(askOrComplete);
-
-program.parse();
-
-//
-// HELPERS
-//
-
-async function askOrComplete(question, options, command) {
-  let clientOptions = loadClientOptions(program.opts().config);
-
-  // console.log("CLIENT OPTIONS", clientOptions);
-
-  if (!question) {
-    question = await promptForQuestion();
+  constructor() {
+    this.program = new Command();
   }
 
-  console.log(chalk.green("\nYou asked: ") + chalk.bold(question));
+  initializeOptions() {
+    // Get the description and version from our own package.json
+    const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+    const program = this.program;
 
-  try {
-    let client = new Polymath(clientOptions);
+    program
+      .name("polymath")
+      .description(packageJson.description)
+      .version(packageJson.version);
 
-    let output;
+    program.option("-d, --debug", "output extra debugging");
+    program.option("-c, --config <path>", "config file");
+    program.addOption(
+      new Option("--openai-api-key <key>", "OpenAI API key").env("OPENAI_API_KEY")
+    );
+    program.option(
+      "-s, --server <endpoint>",
+      "Polymath server endpoint",
+      this.collect.bind(this),
+      []
+    );
+    program.option(
+      "-l, --libraries <libOrDirectory>",
+      "Library files or directory"
+    );
 
-    if (command == "ask") {
-      let results = await client.ask(question);
-      output = results.context();
-    } else {
-      let results = await client.completion(question);
+    program.option("-p, --pinecone", "Use pinecone");
+    program.addOption(
+      new Option("--pinecone-api-key <key>", "pinecone api key").env(
+        "PINECONE_API_KEY"
+      )
+    );
+    program.addOption(
+      new Option("--pinecone-base-url <url>", "pinecone base url").env(
+        "PINECONE_BASE_URL"
+      )
+    );
+    program.addOption(
+      new Option("--pinecone-namespace <namespace>", "pinecone namespace").env(
+        "PINECONE_NAMESPACE"
+      )
+    );
 
-      // console.log("RESULTS: ", results);
+    //
+    // SUB COMMANDS
+    //
 
-      let sources = results.infos
-        ?.map((info) => {
-          return chalk.dim(
-            "Source: " + (info.title || info.description) + "\n" + info.url
-          );
-        })
-        .join("\n\n");
+    program
+      .command("ask")
+      .description("Ask a question to a Polymath")
+      .argument("[question]", "The question to ask")
+      .action(this.askOrComplete.bind(this));
 
-      output = results.completion + "\n\n" + sources;
+    program
+      .command("complete")
+      .description("Ask a Polymath for a completion")
+      .argument("[question]", "The question to ask")
+      .alias("completion")
+      .action(this.askOrComplete.bind(this));
+
+    program.parse();
+
+  }
+
+  //
+  // HELPERS
+  //
+
+  async askOrComplete(question, options, command) {
+    const program = this.program;
+
+    let clientOptions = this.loadClientOptions(program.opts().config);
+
+    // console.log("CLIENT OPTIONS", clientOptions);
+
+    if (!question) {
+      question = await this.promptForQuestion();
     }
 
-    console.log(
-      chalk.green("\nThe Polymath answered with:\n\n  ") + chalk.bold(output)
-    );
-  } catch (e) {
-    console.error(`ERROR: ${e}`);
-  }
-}
+    console.log(chalk.green("\nYou asked: ") + chalk.bold(question));
 
-function debug(message) {
-  if (program.opts().debug) {
-    console.log(chalk.blue(`DEBUG: ${message}`));
-  }
-}
-
-// Hunt around the filesystem for a config file
-function loadClientOptions(configOption) {
-  let rawConfig;
-
-  // if there is a configOption:
-  if (configOption) {
-    // try to load it as a filename
     try {
-      const filename = path.resolve(configOption);
-      debug(`Looking for config at: ${filename}`);
+      let client = new Polymath(clientOptions);
 
-      const config = fs.readFileSync(filename, "utf8");
-      rawConfig = JSON.parse(config);
+      let output;
+
+      if (command == "ask") {
+        let results = await client.ask(question);
+        output = results.context();
+      } else {
+        let results = await client.completion(question);
+
+        // console.log("RESULTS: ", results);
+
+        let sources = results.infos
+          ?.map((info) => {
+            return chalk.dim(
+              "Source: " + (info.title || info.description) + "\n" + info.url
+            );
+          })
+          .join("\n\n");
+
+        output = results.completion + "\n\n" + sources;
+      }
+
+      console.log(
+        chalk.green("\nThe Polymath answered with:\n\n  ") + chalk.bold(output)
+      );
     } catch (e) {
-      // if that fails, try to load ~/.polymath/config/<configOption>.json
+      console.error(`ERROR: ${e}`);
+    }
+  }
+
+  debug(message) {
+    if (this.program.opts().debug) {
+      console.log(chalk.blue(`DEBUG: ${message}`));
+    }
+  }
+
+
+  // Hunt around the filesystem for a config file
+  loadClientOptions(configOption) {
+    let rawConfig;
+
+    const debug = this.debug.bind(this);
+
+    // if there is a configOption:
+    if (configOption) {
+      // try to load it as a filename
       try {
+        const filename = path.resolve(configOption);
+        debug(`Looking for config at: ${filename}`);
+
+        const config = fs.readFileSync(filename, "utf8");
+        rawConfig = JSON.parse(config);
+      } catch (e) {
+        // if that fails, try to load ~/.polymath/config/<configOption>.json
         const homeDir = os.homedir();
         const configPath = path.join(
           homeDir,
@@ -158,14 +172,9 @@ function loadClientOptions(configOption) {
 
         const config = fs.readFileSync(configPath, "utf8");
         rawConfig = JSON.parse(config);
-      } catch (e) {
-        // if that fails, throw an error
-        throw new Error(`Failed to load config at ${configPath}`);
       }
-    }
-  } else {
-    // if that fails, try to load ~/.polymath/config/default.json
-    try {
+    } else {
+      // if that fails, try to load ~/.polymath/config/default.json
       const homeDir = os.homedir();
       const configPath = path.join(
         homeDir,
@@ -173,69 +182,73 @@ function loadClientOptions(configOption) {
         "config",
         "default.json"
       );
+
       debug(`Now, looking for a default config at ${configPath}`);
 
       const config = fs.readFileSync(configPath, "utf8");
       rawConfig = JSON.parse(config);
-    } catch (e) {
-      // if that fails, throw an error
-      throw new Error(`Failed to load default config at ${configPath}`);
     }
+
+    return this.normalizeClientOptions(rawConfig);
   }
 
-  return normalizeClientOptions(rawConfig);
-}
+  // Munge together a clientOptions object from the config file and the command line
+  normalizeClientOptions(rawConfig) {
+    const program = this.program;
 
-// Munge together a clientOptions object from the config file and the command line
-function normalizeClientOptions(rawConfig) {
-  // convert a main host config into the bits needed for the Polymath
-  let clientOptions = {};
-  let programOptions = program.opts();
+    // convert a main host config into the bits needed for the Polymath
+    let clientOptions = {};
+    let programOptions = program.opts();
 
-  clientOptions.apiKey =
-    programOptions.openaiApiKey || rawConfig.default_api_key;
+    clientOptions.apiKey =
+      programOptions.openaiApiKey || rawConfig.default_api_key;
 
-  clientOptions.servers =
-    programOptions.server || rawConfig.client_options.servers || undefined;
+    clientOptions.servers =
+      programOptions.server || rawConfig.client_options.servers || undefined;
 
-  clientOptions.libraryFiles =
-    programOptions.libraries ||
-    rawConfig.client_options?.libraryFiles ||
-    undefined;
+    clientOptions.libraryFiles =
+      programOptions.libraries ||
+      rawConfig.client_options?.libraryFiles ||
+      undefined;
 
-  if (programOptions.pinecone) {
-    clientOptions.pinecone = {
-      apiKey: programOptions.pineconeApiKey,
-      baseUrl: programOptions.pineconeBaseUrl,
-      namespace: programOptions.pineconeNamespace,
-    };
-  } else if (rawConfig.client_options?.pinecone) {
-    clientOptions.pinecone = rawConfig.client_options.pinecone;
+    if (programOptions.pinecone) {
+      clientOptions.pinecone = {
+        apiKey: programOptions.pineconeApiKey,
+        baseUrl: programOptions.pineconeBaseUrl,
+        namespace: programOptions.pineconeNamespace,
+      };
+    } else if (rawConfig.client_options?.pinecone) {
+      clientOptions.pinecone = rawConfig.client_options.pinecone;
+    }
+
+    if (rawConfig.client_options?.omit)
+      clientOptions.omit = rawConfig.client_options.omit;
+
+    if (rawConfig.client_options?.debug)
+      clientOptions.debug = rawConfig.client_options.debug;
+
+    return clientOptions;
   }
 
-  if (rawConfig.client_options?.omit)
-    clientOptions.omit = rawConfig.client_options.omit;
+  // Ask the dear listener for a question as they didn't provide one to the CLI
+  async promptForQuestion() {
+    let question = await inquirer.prompt({
+      type: "input",
+      name: "result",
+      message: "What is your question?",
+    });
+    return question.result;
+  }
 
-  if (rawConfig.client_options?.debug)
-    clientOptions.debug = rawConfig.client_options.debug;
+  // Allowing multiple values for a single option, collecting them in an array
+  collect(value, previous) {
+    return previous.concat([value]);
+  }
 
-  return clientOptions;
 }
 
-// Ask the dear listener for a question as they didn't provide one to the CLI
-async function promptForQuestion() {
-  let question = await inquirer.prompt({
-    type: "input",
-    name: "result",
-    message: "What is your question?",
-  });
-  return question.result;
-}
-
-// Allowing multiple values for a single option, collecting them in an array
-function collect(value, previous) {
-  return previous.concat([value]);
-}
+const cli = new CLI();
+cli.initializeOptions();
 
 // TODO: wrap the main in a function and allow this so folks could load it as a module
 // export function cli(args) {
