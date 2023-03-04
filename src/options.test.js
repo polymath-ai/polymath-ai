d08fa3218c019fb4b639c44333bb8a0d0721e8f5
Dimitri Glazkov <dimitri@glazkov.com>
2023-03-03 21:58:40 -0800

    Add ava for testing.

---
+++ src/options.test.js
@@ -0,0 +1,21 @@
+import test from "ava";
+
+import { Options } from "./options.js";
+
+test("normalizeClientOptions with no program servers", (t) => {
+  const opts = new Options(false);
+  const result = opts.normalizeClientOptions(
+    {},
+    { client_options: { servers: ["config"] } }
+  );
+  t.deepEqual(result, { servers: ["config"] });
+});
+
+test("normalizeClientOptions with no config servers", (t) => {
+  const opts = new Options(false);
+  const result = opts.normalizeClientOptions(
+    { server: ["program"] },
+    { client_options: {} }
+  );
+  t.deepEqual(result, { servers: ["program"] });
+});
