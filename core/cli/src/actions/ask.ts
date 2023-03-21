5dcac26335e2e69e840f2dbe8bd0715cbf0dd81e
Alex Komoroske <jkomoros@gmail.com>
2023-03-20 17:24:25 -0700

    Clean up a few more instances of any

--- core/cli/src/actions/ask.ts
+++ core/cli/src/actions/ask.ts
@@ -1,15 +1,10 @@
 import { Polymath } from "@polymath-ai/client";
-import { Action, RunArguments } from "../action.js";
+import { Action, ActionArgs, RunArguments } from "../action.js";
 
 export class Ask extends Action {
-  opts: any;
-
-  constructor(options: any) {
-    super(options);
-  }
 
   override async run({ args, options, command }: RunArguments): Promise<void> {
-    let question: any = args[0];
+    let question: string = args[0];
     const { debug, error, log } = this.say;
     const clientOptions = this.clientOptions();
 
