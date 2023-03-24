937f117e9a7c091e31d85b9004e58314192d718b
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-24 20:31:27 +0000

    Adding discovery into client.

--- core/client/src/endpoint.ts
+++ core/client/src/endpoint.ts
@@ -5,6 +5,26 @@ import { validateResponse } from "@polymath-ai/validation";
 
 import { AskOptions, PackedLibraryData, Server } from "@polymath-ai/types";
 
+const discoverEndpoint = (url) => {
+  // GET the curent URL/
+  return fetch(url)
+    .then(discoveryResponse => {
+      if (discoveryResponse.ok) {
+        return discoveryResponse.text();
+      }
+    })
+    .then(body => {
+      if (body != undefined) {
+        // This is particularly brittle.
+        const linkMatch = body.match(/<link rel=["']polymath["'] href=["']([^"']+)["']/);
+        if (linkMatch) {
+          // need to work out base.
+          return new URL(linkMatch[1], url);
+        }
+      }
+    });
+};
+
 //
 // Talk to remote servers and ask for their bits
 //
@@ -49,7 +69,28 @@ class PolymathEndpoint {
         method: "POST",
         body: form,
       })
-    ).json();
+    )
+    .then((response: Response): Promise<Response> => {
+      if (
+        response.ok == false ||
+        response.headers["content-type"] != "application/json"
+      ) {
+        // There was an error, so attempt a GET to see if there is a link rel.
+        return discoverEndpoint(url)
+          .then((newUrl: string): Promise<Response> => { return fetch(newUrl, { method: "POST", body: form })});
+      }
+      return response;
+    })
+    .then((response: Response): Promise<Response> => {
+      if (response.ok == false) {
+        throw new Error("Server responded with " + response.status);
+      }
+      return response;
+    })
+    .then((response: Response) => {
+      return response.json();
+    })
+  );
 
     return validateResponse(result);
   }
