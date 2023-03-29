7b9f8eabc2d7b812cbf2e67e3808191426259e39
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-29 18:44:12 +0000

    Fixing the import.

--- core/ingest/index.ts
+++ core/ingest/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-export { Ingester } from "./src/importer.js";
+export { Ingester } from "./src/ingester.js";
 export { Bit, Options } from "./src/types.js";
 export { Ingest, IngestArguments, IngestOptions } from "./src/main.js";
