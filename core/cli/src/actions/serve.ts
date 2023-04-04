e8cbaeade7249020750b5a8cad594fd651da08ac
Dimitri Glazkov <dimitri@glazkov.com>
2023-04-03 21:19:09 -0700

    Add `express` as deps to `core/cli`.

--- core/cli/src/actions/serve.ts
+++ core/cli/src/actions/serve.ts
@@ -1,12 +1,24 @@
 import { Action } from "../action.js";
 import { ServeArgs } from "@polymath-ai/types";
 
+import express from "express";
+
+const PORT = 3000;
+
 export class Serve extends Action {
   override async run(opts: ServeArgs): Promise<void> {
-    const { debug, error, log } = this.say;
-    return new Promise((resolve) => {
-      console.log("SERVE!");
-      resolve();
+    const { log } = this.say;
+    return new Promise(() => {
+      log("SERVE!", JSON.stringify(opts, null, 2));
+
+      const app = express();
+
+      app.post("/", (request, response) => {
+        log("request", JSON.stringify(request, null, 2));
+        response.send("Hello from Express!");
+      });
+
+      app.listen(PORT);
     });
   }
 }
