4f34241b51eaea0078a5b4e99dfac8e07c6c1b4f
Dimitri Glazkov <dimitri@glazkov.com>
2023-04-13 21:42:18 -0700

    Remove `ask` method.
    It was confusing.

--- core/host/src/host.ts
+++ core/host/src/host.ts
@@ -1,12 +1,7 @@
 import { AskOptions, LibraryData, PackedLibraryData } from "@polymath-ai/types";
-import { encodeEmbedding, fromFormData } from "./utils.js";
+import { encodeEmbedding } from "./utils.js";
 
 export abstract class PolymathHost {
-  async ask(formData: FormData): Promise<PackedLibraryData> {
-    const args = fromFormData(formData);
-    return this.queryPacked(args);
-  }
-
   async queryPacked(args: AskOptions): Promise<PackedLibraryData> {
     const results = await this.query(args);
 
