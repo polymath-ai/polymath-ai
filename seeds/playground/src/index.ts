5c89688e59f1315dedb81d956626051bce015326
Dimitri Glazkov <dimitri@glazkov.com>
2023-05-01 19:29:32 -0700

    Add command line args support.

--- seeds/playground/src/index.ts
+++ seeds/playground/src/index.ts
@@ -1,14 +1,15 @@
 import { openai, Prompts } from "@polymath-ai/ai";
 import { intro, text, outro, spinner, confirm } from "@clack/prompts";
 import { config } from "dotenv";
+import { Command } from "commander";
 
 config();
 
-const root = new URL("../../prompts", import.meta.url);
+const root = new URL("../../", import.meta.url);
 const prompts = new Prompts(root.pathname);
 const QUIT_VALUE = "<quit>";
 
-const cycle = async () => {
+const cycle = async (promptFile: string) => {
   const question = (await text({
     message: "Type a message or hit <Enter> to exit",
     defaultValue: QUIT_VALUE,
@@ -20,7 +21,7 @@ const cycle = async () => {
     return shouldQuit;
   }
 
-  const prompt = prompts.get("situation", { question });
+  const prompt = prompts.get(promptFile, { question });
   const s = spinner();
   s.start("Generating text...");
   const response = await fetch(
@@ -34,8 +35,14 @@ const cycle = async () => {
   s.stop(reply);
 };
 
-intro("Let's experiment.");
+const program = new Command();
+program
+  .command("cycle")
+  .argument("[prompt]", "The prompt file to use while cycling")
+  .action(async (prompt: string) => {
+    intro("Let's experiment.");
+    while (!(await cycle(prompt)));
+    outro("Awesome experimenting!");
+  });
+program.parse();
 
-while (!(await cycle()));
-
-outro("Awesome experimenting!");
