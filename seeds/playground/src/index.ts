5979b15d5b4aa29f9450d9c7a7a5f9f11a85a475
Dimitri Glazkov <dimitri@glazkov.com>
2023-05-02 21:48:50 -0700

    Add diverge-converge reasoning.

--- seeds/playground/src/index.ts
+++ seeds/playground/src/index.ts
@@ -9,13 +9,8 @@ const root = new URL("../../", import.meta.url);
 const prompts = new Prompts(root.pathname);
 const QUIT_VALUE = "<quit>";
 
-const ask = async (
-  promptFile: string,
-  context: Record<string, string>
-): Promise<string> => {
+const single = async (promptFile: string, context: Record<string, string>) => {
   const prompt = prompts.get(promptFile, context);
-  const s = spinner();
-  s.start("Generating text...");
   const response = await fetch(
     openai(process.env.OPENAI_API_KEY).completion({
       prompt,
@@ -23,8 +18,59 @@ const ask = async (
     })
   );
   const result = await response.json();
-  const reply = result.choices[0].text.trim();
+  return result.choices[0].text.trim();
+};
+
+const ask = async (
+  promptFile: string,
+  context: Record<string, string>
+): Promise<string> => {
+  const s = spinner();
+  s.start("Generating text...");
+  const reply = await single(promptFile, context);
   s.stop(reply);
+  return reply;
+};
+
+const diamond = async (
+  divergePromptFile: string,
+  convergePromptFile: string,
+  spread: number,
+  context: Record<string, string>
+) => {
+  let prompt = prompts.get(divergePromptFile, context);
+  const model = "text-davinci-003";
+  const divergeSpinner = spinner();
+  divergeSpinner.start("Generating divergent ideas ...");
+  const responses = await Promise.all(
+    Array.from({ length: spread }).map(() => {
+      return fetch(
+        openai(process.env.OPENAI_API_KEY).completion({
+          prompt,
+          model,
+        })
+      );
+    })
+  );
+  const data = await Promise.all(responses.map((r) => r.json()));
+  const replyList = data.map((d) => d.choices[0].text.trim());
+  const replies = replyList
+    .map((reply, i) => `== REPLY ${i} ==\n${reply}`)
+    .join("\n");
+  divergeSpinner.stop(replies);
+
+  const convergeSpinner = spinner();
+  convergeSpinner.start("Converging ...");
+  prompt = prompts.get(convergePromptFile, { ...context, replies });
+  const response = await fetch(
+    openai(process.env.OPENAI_API_KEY).completion({
+      prompt,
+      model,
+    })
+  );
+  const result = await response.json();
+  const reply = result.choices[0].text.trim();
+  convergeSpinner.stop(reply);
   return reply;
 };
 
@@ -55,5 +101,23 @@ program
     while (!(await cycle(prompts)));
     outro("Awesome experimenting!");
   });
+
+program
+  .command("diamond")
+  .argument("[prompt...]", "The prompt file(s) to use while diamonding")
+  .action(async (prompts: string[]) => {
+    intro("Let's diamond.");
+
+    const question = (await text({
+      message: "Type a message or hit <Enter> to exit",
+      defaultValue: QUIT_VALUE,
+    })) as string;
+    if (question === QUIT_VALUE) return;
+
+    await diamond(prompts[0], prompts[1], 5, { question });
+    outro("Awesome diamonding!");
+  }); 
+
+
 program.parse();
 
