216f45e3aa580b3ea865ca0b2af5441c2cd54112
Dimitri Glazkov <dimitri@glazkov.com>
2023-05-21 21:56:26 -0700

    Wire up inputs.

--- seeds/playground/src/web/playground.ts
+++ seeds/playground/src/web/playground.ts
@@ -10,6 +10,7 @@ import {
   ViewUpdate,
 } from "@codemirror/view";
 import { Prompt } from "./prompt.js";
+import { Input } from "./input.js";
 
 class PlaceholderWidget extends WidgetType {
   name: string;
@@ -93,8 +94,13 @@ export class Playground extends HTMLElement {
         /* .cm-editor { height: 500px; } */
         .cm-scroller { overflow: auto }
 
+        #inputs, #controls {
+          padding: 10px;
+        }
+
         #editor {
           display: flex;
+          overflow: auto;
         }
 
         #editor, pre {
@@ -103,10 +109,12 @@ export class Playground extends HTMLElement {
         }
 
         pre {
+          padding: 10px;
           overflow: auto;
         }
       </style>
       <div id="playground">
+        <div id="inputs"></div>
         <div id="panels">
           <div id="editor"></div>
           <pre><code id="output"></code></pre>
@@ -119,15 +127,13 @@ export class Playground extends HTMLElement {
     const outputElement = root.getElementById("output") as HTMLElement;
     runButton.addEventListener("click", async () => {
       runButton.disabled = true;
-      const response = await this.run({
-        question: "Will it rain tomorrow?",
-      });
+      const response = await this.run(this.getInputValues());
       outputElement.textContent = response;
       runButton.disabled = false;
     });
   }
 
-  connectedCallback() {
+  createEditor() {
     // TODO: Make work with more than one prompt.
     const prompt = this.querySelector("ai-prompt") as Prompt;
     if (!prompt) {
@@ -149,6 +155,30 @@ export class Playground extends HTMLElement {
       ],
       parent: editorElement,
     });
+  }
+
+  createInputs() {
+    const inputsElement =
+      (this.shadowRoot?.querySelector("#inputs") as HTMLElement) || undefined;
+    if (!inputsElement) return;
+    const aiInputs = Array.from(this.querySelectorAll("ai-input")) as Input[];
+    aiInputs.forEach((aiInput: Input) => {
+      const inputElement = document.createElement("input");
+      inputElement.type = "text";
+      inputElement.name = aiInput.name || "";
+      inputElement.placeholder = aiInput.name || "";
+      inputsElement.appendChild(inputElement);
+    });
+  }
+
+  getInputValues() {
+    const inputs = Array.from(this.shadowRoot?.querySelectorAll("input") || []);
+    return Object.fromEntries(inputs.map((input) => [input.name, input.value]));
+  }
+
+  connectedCallback() {
+    this.createEditor();
+    this.createInputs();
   }
 
   async run(params: Record<string, string> = {}) {
